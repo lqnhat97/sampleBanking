8968d288dd354c0a7c5ff97861f08cfeb2ca38cd
nhatlq <lqnhat97@gmail.com>
2019-01-07 07:27:53 +0700

    update api

--- vue/sampleBanking/src/components/DirectoryPage.vue
+++ vue/sampleBanking/src/components/DirectoryPage.vue
@@ -14,7 +14,7 @@
                 <th>Xóa</th>
               </thead>
               <tbody>
-                <tr v-for="el in directory" v-bind:key="el.idRootUser">
+                <tr v-for="el in directory" v-bind:key="el.stkElementUser">
                   <td>{{el.stkElementUser}}</td>
                   <td>{{el.elementUserName}}</td>
                   <td>{{(el.idBank==777)?'iBank':((el.idBank==999)?'NBank':'PPNBank')}}</td>
@@ -29,6 +29,8 @@
                 </tr>
               </tbody>
             </table>
+            <p data-placement="top" data-toggle="tooltip" title="Add"><button class="btn btn-primary" data-title="Add" 
+            data-toggle="modal" data-target="#add">Thêm danh bạ</button>
             <div class="clearfix"></div>
           </div>
         </div>
@@ -41,24 +43,15 @@
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove"
                 aria-hidden="true"></span></button>
-            <h4 class="modal-title custom_align" id="Heading">Edit Your Detail</h4>
+            <h4 class="modal-title custom_align" id="Heading">Chỉnh sửa danh bạ</h4>
           </div>
           <div class="modal-body">
             <div class="form-group">
-              <input class="form-control " type="text" placeholder="Mohsin">
-            </div>
-            <div class="form-group">
-
-              <input class="form-control " type="text" placeholder="Irshad">
-            </div>
-            <div class="form-group">
-              <textarea rows="2" class="form-control" placeholder="CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan"></textarea>
-
-
+              <input class="form-control" v-model="elementUserName" type="text" placeholder="Tên">
             </div>
           </div>
           <div class="modal-footer ">
-            <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
+            <button type="button" @click="changeName" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
           </div>
         </div>
         <!-- /.modal-content -->
@@ -75,13 +68,36 @@
           </div>
           <div class="modal-body">
 
-            <div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span> Are you sure you
-              want to delete this Record?</div>
+            <div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span> Bạn có muốn xóa không?</div>
 
           </div>
           <div class="modal-footer ">
-            <button type="button" class="btn btn-success"><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
-            <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
+            <button type="button" @click="deleteDirectory" class="btn btn-success"><span class="glyphicon glyphicon-ok-sign"></span>Có</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span>Không</button>
+          </div>
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+    </div>
+    <div class="modal fade" id="add" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove"
+                aria-hidden="true"></span></button>
+            <h4 class="modal-title custom_align" id="Heading">Thêm danh bạ</h4>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <input class="form-control" v-model="addStk" type="number" placeholder="Số tài khoản">
+            </div>
+            <div class="form-group">
+              <input class="form-control" v-model="addUserName" type="text" placeholder="Tên">
+            </div>
+          </div>
+          <div class="modal-footer ">
+            <button type="button" @click="addDirectory" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span>Thêm</button>
           </div>
         </div>
         <!-- /.modal-content -->
@@ -96,13 +112,73 @@ import axios from 'axios';
   export default {
     data(){
       return{
-        directory:''
+        directory:'',
+        elementUserName:'',
+        addUserName:'',
+        addStk:''
       }
     },
     props: {
       id: {
         type: Number,
         required: false
+      }
+    },
+    methods:{
+      changeName(e){
+        e.preventDefault();
+        axios({
+        method: 'POST',
+        headers: {
+        'x-access-token': JSON.parse(localStorage.getItem("user")).data.access_token,
+        'Content-Type': 'application/json'
+        },
+        url: 'http://192.168.0.142:8088/api/directory/updateDirectory',
+        data:{
+          elementUserName:this.elementUserName,
+          idRootUser:this.id
+        }
+        }).then(value=>{
+          
+      })
+      },
+    deleteDirectory(e){
+        e.preventDefault();
+        axios({
+        method: 'POST',
+        headers: {
+        'x-access-token': JSON.parse(localStorage.getItem("user")).data.access_token,
+        'Content-Type': 'application/json'
+        },
+        url: 'http://192.168.0.142:8088/api/directory/deleteDirectory',
+        data:{
+          stkElementUser:this.elementUserName,
+          idRootUser:this.id
+        }
+        }).then(value=>{
+          console.log(value);
+
+      })
+      },
+      addDirectory(e){
+        e.preventDefault();
+        axios({
+        method: 'POST',
+        headers: {
+        'x-access-token': JSON.parse(localStorage.getItem("user")).data.access_token,
+        'Content-Type': 'application/json'
+        },
+        url: 'http://192.168.0.142:8088/api/directory/createDirectory',
+        data:{
+          idRootUser:this.id,
+          idBank:777,
+          stkElementUser:this.addStk,
+          elementUserName:this.addUserName
+        }
+        }).then(value=>{
+          console.log(value);
+
+      })
       }
     },
     created(){
--- vue/sampleBanking/src/components/TransferPage.vue
+++ vue/sampleBanking/src/components/TransferPage.vue
@@ -3,7 +3,6 @@
     <div class="contact-image">
       <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact" />
     </div>
-    <form>
       <h3>Chuyển khoản</h3>
       <div class="row">
         <div class="col-md-6">
@@ -11,10 +10,10 @@
             <input type="number" v-model="stkNhan" @change="findInfo" name="txtSTT" class="form-control" placeholder="Số tài khoản *" value="" />
           </div>
           <div class="form-group">
-            <input type="number" name="txtTien" class="form-control" placeholder="Số tiền" value="" />
+            <input type="number" v-model="soTien" name="txtTien" class="form-control" placeholder="Số tiền" value="" />
           </div>
           <div class="form-group">
-            <input type="text" name="txtNoidung" class="form-control" placeholder="Nội dung chuyển khoản" value="" />
+            <input type="text" name="txtNoidung" v-model="content" class="form-control" placeholder="Nội dung chuyển khoản" value="" />
           </div>
         </div>
         <div class="col-md-6">
@@ -28,11 +27,32 @@
             <input name="txtSDT" class="form-control" v-model="dienthoai" placeholder="Số điện thoại" readonly>
           </div>
                     <div class="form-group">
-            <input type="submit" name="btnSubmit" class="btn btn-success" value="Chuyển khoản" />
+            <p data-placement="top" data-toggle="tooltip" title="Add"><button class="btn btn-primary" data-title="Add" 
+            data-toggle="modal" data-target="#transfer" @click="sendOTP">Chuyển khoản</button></p>
           </div>
         </div>
       </div>
-    </form>
+      <div class="modal fade" id="transfer" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove"
+                aria-hidden="true"></span></button>
+            <h4 class="modal-title custom_align" id="Heading">Nhập OTP</h4>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <input class="form-control" v-model="optValue" type="text" placeholder="OTP">
+            </div>
+          </div>
+          <div class="modal-footer ">
+            <button type="button"  class="btn btn-warning btn-lg" @click="transferDKM" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span>Chuyển khoản</button>
+          </div>
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+    </div>
   </div>
 </template>
 
@@ -45,7 +65,10 @@ import axios from 'axios';
         stkNhan:'',
         userName:'',
         email:'',
-        dienthoai:''
+        dienthoai:'',
+        optValue:'',
+        soTien:'',
+        content:''
       }
     },
     props: {
@@ -74,6 +97,64 @@ import axios from 'axios';
             this.userName=value.data[0].userName;
             this.email=value.data[0].email;
             this.dienthoai=value.data[0].dienthoai;
+        })
+      },
+      sendOTP(){
+        axios({
+          method: 'post',
+          headers: {
+          'x-access-token': JSON.parse(localStorage.getItem("user")).data.access_token,
+          'Content-Type': 'application/json'
+          },
+          url: 'http://192.168.0.142:8088/api/otpmail/sendOtpMail',
+          data:{
+            userName:JSON.parse(localStorage.getItem("user")).data.user.userName,
+            email:JSON.parse(localStorage.getItem("user")).data.user.email,
+          }
+          }).then(value=>{
+            this.userName=value.data[0].userName;
+            this.email=value.data[0].email;
+            this.dienthoai=value.data[0].dienthoai;
+        })
+      },
+      transferDKM(){
+        axios({
+          method: 'post',
+          headers: {
+          'x-access-token': JSON.parse(localStorage.getItem("user")).data.access_token,
+          'Content-Type': 'application/json'
+          },
+          url: 'http://192.168.0.142:8088/api/otpmail/checkOtp',
+          data:{
+            userName:JSON.parse(localStorage.getItem("user")).data.user.userName,
+            token:this.optValue,
+          }
+          }).then(value=>{
+            console.log(value);
+            if(value.status == 200){
+              axios({
+                method: 'post',
+                headers: {
+                'x-access-token': JSON.parse(localStorage.getItem("user")).data.access_token,
+                'Content-Type': 'application/json'
+                },
+                url: 'http://192.168.0.142:8088/api/transfer/internal',
+                data:{
+                  SoTaiKhoanGui:this.stk,
+                  SoTaiKhoanNhan:this.stkNhan,
+                  amount:this.soTien,
+                  transactionFee:1000,
+                  content:this.content,
+                  idBank:777,
+                  typeFee:0
+                }
+              }).then(()=>{
+                alert('Đã chuyển tiền');
+              })
+            }
+            else{
+              alert("Sai OTP hoặc OTP hết hạn");
+            }
         })
       }
     }
